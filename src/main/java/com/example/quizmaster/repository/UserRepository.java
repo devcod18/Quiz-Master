0bcfbab762b28268d58bd7a12b22c0146b340991
Shahob <sanjarbeksultanbayev45@gmail.com>
2024-10-09 13:36:07 +0500

    sdfg

--- src/main/java/com/example/quizmaster/repository/UserRepository.java
+++ src/main/java/com/example/quizmaster/repository/UserRepository.java
@@ -12,13 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-
-    boolean existsByEmail(String email);
-
-    User findByActivationCode(Integer code);
-
-    Page<User> findAllByRole(RoleEnum role, PageRequest pageRequest);
 
     Optional<User> findById(Long id);
 
